8fe44b6ec7e5413f4ca1fbf970a99bee5496ca92
Francesco Fevoli <francesdbz18pc@outlook.es>
2024-10-08 14:13:43 +0200

    not working

--- app/src/main/java/com/chesco/diceroller/MainActivity.kt
+++ app/src/main/java/com/chesco/diceroller/MainActivity.kt
@@ -5,7 +5,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -58,17 +60,90 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         5 -> R.drawable.dice_5
         else -> R.drawable.dice_6
     }
-    Column(
-        modifier = modifier,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Image(
-            painter = painterResource(imageResource),
-            contentDescription = result.toString()
-        )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { result = (1..6).random() }) {
-            Text(stringResource(R.string.roll))
+    var result2 by remember { mutableStateOf(1) }
+    val imageResource2 = when (result2) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    var result3 by remember { mutableStateOf(1) }
+    val imageResource3 = when (result3) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    var result4 by remember { mutableStateOf(1) }
+    val imageResource4 = when (result4) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+    Column (modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally){
+        Row(horizontalArrangement= Arrangement.SpaceEvenly) {
+            Column(
+                modifier = modifier,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(imageResource),
+                    contentDescription = result.toString()
+                )
+                Spacer(modifier = Modifier.height(6.dp))
+                Button(onClick = { result = (1..6).random() }) {
+                    Text(stringResource(R.string.roll))
+                }
+            }
+            Column(
+                modifier = modifier,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(imageResource2),
+                    contentDescription = result2.toString()
+                )
+                Spacer(modifier = Modifier.height(6.dp))
+                Button(onClick = { result2 = (1..6).random() }) {
+                    Text(stringResource(R.string.roll))
+                }
+            }
+        }
+        Row(horizontalArrangement= Arrangement.SpaceEvenly) {
+            Column(
+                modifier = modifier,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(imageResource3),
+                    contentDescription = result3.toString()
+                )
+                Spacer(modifier = Modifier.height(6.dp))
+                Button(onClick = { result3 = (1..6).random() }) {
+                    Text(stringResource(R.string.roll))
+                }
+            }
+            Column(
+                modifier = modifier,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(imageResource4),
+                    contentDescription = result4.toString()
+                )
+                Spacer(modifier = Modifier.height( 6.dp))
+                Button(onClick = { result4 = (1..6).random() }) {
+                    Text(stringResource(R.string.roll))
+                }
+            }
         }
     }
+
 }
